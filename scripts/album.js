a50356cb86af8c2d610ab9dfbf7696c9425d4135
Scott-Moore <scott.moore2016@yahoo.com>
2017-01-25 19:47:14 -0700

    checkpoint-11-album

---
+++ scripts/album.js
@@ -0,0 +1,88 @@
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUr1: 'assets/images/01.png',
+  songs: [
+    { title: 'Blue', duration: '4:26'},
+    { title: 'Green', duration: '3:14'},
+    { title: 'Red', duration: '5:01'},
+    { title: 'Pink', duration: '3:21'},
+    { title: 'Magenta', duration: '2:15'},
+  ]
+};
+
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUr1: 'assets/images/20.png',
+  songs: [
+    {title: 'Hello, Operator?', duration: '1:01'},
+    {title: 'Ring, ring, ring', duration: '5:01'},
+    {title: 'Fits in your pocket', duration: '3:21'},
+    {title: 'Can you hear me now?', duration: '3:14'},
+    {title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+
+var albumAristotle = {
+  title: 'Nichomachean Ethics',
+  artist: 'Aristotle',
+  label: 'Philosopher Kings',
+  year: '384 bc',
+  albumArtUr1:'assets/images/17.png',
+  songs: [
+    {title: 'Stagira', duration: '4:35'},
+    {title: 'Kings of Macedonia', duration: '5:48'},
+    {title: 'Platos Demise', duration: '2:28'},
+    {title: 'Athens Escape', duration: '13:21'},
+    {title: 'The Art of Rhetoric', duration: '8:10'}
+  ]
+};
+
+var createSongRow = function(songNumber, songName, songLength) {
+  var template =
+    '<tr class="album-view-song-item">'
+    +' <td class="song-item-number">' + songNumber + '</td>'
+    +' <td class="song-item-title">' + songName + '</td>'
+    +' <td class="song-item-duration>' +songLength '</td>'
+   +'</tr>'
+   ;
+  return template;
+};
+
+  var albumTitle = document.getElementsByClassName('album-view-title')[0];
+  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+  var albumReleaseInfo = document.getElementsbyClassName('album-view-release-info')[0];
+  var albumImage = document.getElementsByClassName('album-cover-art')[0];
+  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var setCurrentAlbum = function(album) { 
+  albumTitle.firstChild.nodeValue = album.title;
+  albumArtist.firstChile.nodeValue = album.artist;
+  albumReleaseInfo.firstChile.nodeValue = album.year + '' + album.label;
+  albumImage.setAttribute('src',albumArtUrl);
+  
+  albumSongList.innerHTML = '';
+  
+  for (var i = 0; i < album.songs.length; i++) {
+    albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+  }
+  
+};
+
+window.onload = function(){
+  setCurrentAlbum(albumPicasso);
+  
+var albums =[albumPicasso,ablbumMarconie,albumAristotle];
+var index = 1;
+  album.image.addEventListener("click",function(event) {  
+    setCurrentAlbum(album[index]);
+    index++;
+    if (index == albums.length){
+      index = 0;
+    }
+  });
+};
